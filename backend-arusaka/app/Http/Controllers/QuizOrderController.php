b0229a97820819cecb4b4c61cc05121806c95d2b
rickys20 <rickysupriyanto807@gmail.com>
2023-08-20 19:39:01 +0700

    feat:update system start quiz

--- backend-arusaka/app/Http/Controllers/QuizOrderController.php
+++ backend-arusaka/app/Http/Controllers/QuizOrderController.php
@@ -3,24 +3,115 @@
 namespace App\Http\Controllers;
 
 use App\Models\QuizOrder;
+use App\Models\QuizItem;
+use App\Models\Quiz;
+use App\Models\MaterialOrder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class QuizOrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function getQuiz(Request $request, $quiz)
     {
-        //
+        $dataQuiz = Quiz::where('slug', $quiz)->first();
+        $checkauth = auth()->user();
+        if (!$checkauth) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+        if ($dataQuiz) {
+            // cek materialnya sudah memenuhi minimum syarat belum
+            $dataMaterialOrder = MaterialOrder::where('courses_id', $dataQuiz->courses_id)
+                ->where('user_id', $checkauth->id)
+                ->where('status', 'unlocked')
+                ->get();
+            $jumlahData = $dataMaterialOrder->count();
+            if ($jumlahData >= $dataQuiz->minimum_material) {
+                // cek data jawab soal ini sudah ada belom ?
+                $dataQuizOrderExist = QuizOrder::where('quiz_id', $dataQuiz->id)->where('user_id', $checkauth->id)->first();
+                if ($dataQuizOrderExist) {
+                    return response()->json([
+                        'status' => 'Old data',
+                        'message' => $dataQuizOrderExist
+                    ], 200);
+                } else {
+                    $new_order = new QuizOrder();
+                    $new_order->user_id = $checkauth->id;
+                    $new_order->quiz_id = $dataQuiz->id;
+
+                    // dapatkan id soal dalam array
+                    $dataNoSoal = QuizItem::where('quiz_id', $dataQuiz->id)->pluck('id')->toArray();
+                    $jumlahData = count($dataNoSoal);
+                    $SoalString = '[' . implode(',', $dataNoSoal) . ']';
+                    $new_order->question = $SoalString;
+
+                    $answers = array_fill(0, $jumlahData, "0");
+                    $StringAnswer = '[' . implode(',', $answers) . ']';
+                    $new_order->answers = $StringAnswer;
+                    $new_order->save();
+
+                    return response()->json([
+                        'message' => $new_order
+                    ], 200);
+                }
+            } else {
+                return response()->json([
+                    // harus menyelesaikan minimum material
+                    'message' => 'Quiz not available'
+                ], 400);
+            }
+        } else {
+            return response()->json([
+                'message' => 'Data Quiz not found'
+            ], 400);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    // akses data quiz per soal
+    public function getQuizPerNum(Request $request, $quiz, $number)
     {
-        //
+        $dataQuiz = Quiz::where('slug', $quiz)->first();
+        $checkauth = auth()->user();
+        if (!$checkauth) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+        $dataQuizOrderExist = QuizOrder::where('quiz_id', $dataQuiz->id)->where('user_id', $checkauth->id)->first();
+        if ($dataQuizOrderExist) {
+            $id_item = $dataQuizOrderExist->question;
+            $numbers_data = array_map('intval', explode(',', trim($id_item, '[]')));
+            $data_persoal = QuizItem::select('content', 'a', 'b', 'c', 'd')->where('id', $numbers_data[$number - 1])->first();
+            return response()->json([
+                'message' => $data_persoal
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Quiz Not Found'
+            ], 200);
+        }
+    }
+
+    public function submitAnswer(Request $request, $quiz, $number)
+    {
+        $checkauth = auth()->user();
+        if (!$checkauth) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        };
+        $data_slug = Quiz::where('slug', $quiz)->first();
+        $data_order = QuizOrder::where('user_id', $checkauth->id)
+            ->where('quiz_id', $data_slug->id)
+            ->first();
+        $id_item = $data_order->answers;
+
+        // ubah data text jadi array
+        $jawaban = json_decode($id_item);
+        $jawaban[$number- 1] = $request->answers;
+
+        $data_order->update([
+            'answers' => $jawaban
+        ]);
+        return response()->json([
+            'data_item_change' => $jawaban,
+        ]);
     }
 
     /**
